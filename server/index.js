0d4687893390030e55e9a1072800a4a006e3af56
Paul Okello <paulokello273@gmail.com>
2021-01-02 14:03:14 +0300

    🔩🔩🔩getRules🔩🔩🔩

--- server/index.js
+++ server/index.js
@@ -5,3 +5,31 @@ const TOKEN = process.env.TWITTER_BEARER_TOKEN;
 const rulesURL = "https://api.twitter.com/2/tweets/search/stream/rules";
 const streamURL =
   "https://api.twitter.com/2/tweets/search/stream?tweet.field=public_metrics&expansions=author_id,attachments.media_keys,referenced_tweets.id,geo.place_id";
+
+const rules = [
+  {
+    value: "FPL",
+  },
+];
+
+//   GET stream rules
+async function getRules() {
+  const response = await needle("get", rulesURL, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+
+  console.log(response.body);
+  return response.body;
+}
+
+(async () => {
+  let currentRules;
+  try {
+    currentRules = await getRules();
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+})();
